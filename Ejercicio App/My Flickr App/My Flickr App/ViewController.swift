9a7ddb26949248f99fcecf114d5e6031bd1b30a0
Mauricio Domenech <mauriciodomenech@icloud.com>
2018-03-06 18:04:50 -0300

    Creación del proyecto

---
+++ Ejercicio App/My Flickr App/My Flickr App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Flickr App
+//
+//  Created by Mauricio Domenech on 06/03/2018.
+//  Copyright © 2018 Mauricio Domenech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
